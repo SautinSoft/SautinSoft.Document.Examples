3b305d81912f532b0ab5492453e3666fe20676ab
Max Sautin <info@sautinsoft.net>
2022-04-14 08:32:03 +0400

    Updated Code Examples. April 14th 2022.

--- CSharp/01. Document/01. Document Builder/01. Inserting a Table/Sample.cs
+++ CSharp/01. Document/01. Document Builder/01. Inserting a Table/Sample.cs
@@ -1,59 +1,87 @@
+using System;
 using SautinSoft.Document;
+using SautinSoft.Document.Tables;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace Sample
+namespace Example
 {
-    class Sample
+    class Program
     {
         static void Main(string[] args)
         {
-            TextColumns();
+            InsertingTable();
         }
-      
-		/// <summary>
-        /// Working with text columns. 
+        /// <summary>
+        /// Creates a table using DocumentBuilder and saves it in a desired format.
         /// </summary>
         /// <remarks>
-        /// Details: https://sautinsoft.com/products/document/help/net/developer-guide/text-columns.php
+        /// Details: https://www.sautinsoft.com/products/document/help/net/developer-guide/documentbuilder-inserting-table.php
         /// </remarks>
-        public static void TextColumns()
+
+        static void InsertingTable()
         {
-            string documentPath = @"TextColumns.docx";
-
-            // Let's create a document with 4 columns.
             DocumentCore dc = new DocumentCore();
+            DocumentBuilder db = new DocumentBuilder(dc);
 
-            // Add new section
-            Section s = new Section(dc);
-            dc.Sections.Add(s);
+            // Create a new table with preferred width.
+            Table table = db.StartTable();
+            db.TableFormat.PreferredWidth = new TableWidth(LengthUnitConverter.Convert(5, LengthUnit.Inch, LengthUnit.Point), TableWidthUnit.Point);
 
-            s.PageSetup.PageMargins = new PageMargins()
-            {
-                Top = LengthUnitConverter.Convert(5, LengthUnit.Millimeter, LengthUnit.Point),
-                Right = LengthUnitConverter.Convert(5, LengthUnit.Millimeter, LengthUnit.Point),
-                Bottom = LengthUnitConverter.Convert(5, LengthUnit.Millimeter, LengthUnit.Point),
-                Left = LengthUnitConverter.Convert(5, LengthUnit.Millimeter, LengthUnit.Point)
-            };
+            // Specify formatting of cells and alignment.
+            db.CellFormat.Borders.SetBorders(MultipleBorderTypes.Outside, BorderStyle.Single, Color.Green, 1);
+            db.CellFormat.VerticalAlignment = VerticalAlignment.Top;
+            db.ParagraphFormat.Alignment = HorizontalAlignment.Center;
 
-            s.PageSetup.TextColumns = new TextColumnCollection(4);
-            s.PageSetup.TextColumns.EvenlySpaced = false;
-            s.PageSetup.TextColumns[0].Width = LengthUnitConverter.Convert(60, LengthUnit.Millimeter, LengthUnit.Point);
-            s.PageSetup.TextColumns[1].Width = LengthUnitConverter.Convert(20, LengthUnit.Millimeter, LengthUnit.Point);
-            s.PageSetup.TextColumns[2].Width = LengthUnitConverter.Convert(60, LengthUnit.Millimeter, LengthUnit.Point);
-            s.PageSetup.TextColumns[3].Width = LengthUnitConverter.Convert(20, LengthUnit.Millimeter, LengthUnit.Point);
+            // Specify height of rows and write text.
+            db.RowFormat.Height = new TableRowHeight(105f, HeightRule.Exact);
+            db.InsertCell();
+            db.Write("This is Row 1 Cell 1");
+            db.InsertCell();
+            db.Write("This is Row 1 Cell 2");
+            db.InsertCell();
+            db.Write("This is Row 1 Cell 3");
+            db.EndRow();
 
-            // Fill our columns by any text.
-            string text = "Shrek and Donkey arrive at Farquaad's palace in Duloc, where they end up in a tournament. The winner gets the \"privilege\" of rescuing Fiona so that Farquaad may marry her. ";
+            // Specify formatting of cells and alignment.
+            db.CellFormat.Borders.SetBorders(MultipleBorderTypes.Outside, BorderStyle.Single, Color.Black, 1);
+            db.CellFormat.VerticalAlignment = VerticalAlignment.Center;
+            db.ParagraphFormat.Alignment = HorizontalAlignment.Left;
 
-            for (int i = 0; i < 22; i++)
-                s.Content.End.Insert(text, new CharacterFormat() { FontName = "Arial", Size = 12 });
+            // Specify height of rows and write text.
+            db.RowFormat.Height = new TableRowHeight(150f, HeightRule.Exact);
+            db.InsertCell();
+            db.Write("This is Row 2 Cell 1");
+            db.InsertCell();
+            db.Write("This is Row 2 Cell 2");
+            db.InsertCell();
+            db.Write("This is Row 2 Cell 3");
+            db.EndRow();
 
-            (s.Blocks[0] as Paragraph).ParagraphFormat.Alignment = HorizontalAlignment.Justify;
+            // Specify formatting of cells and alignment.
+            db.CellFormat.Borders.SetBorders(MultipleBorderTypes.Outside, BorderStyle.Single, Color.Orange, 1);
+            db.CellFormat.VerticalAlignment = VerticalAlignment.Bottom;
+            db.ParagraphFormat.Alignment = HorizontalAlignment.Right;
+
+            // Specify height of rows and write text
+            db.RowFormat.Height = new TableRowHeight(125f, HeightRule.Exact);
+            db.InsertCell();
+            db.Write("This is Row 3 Cell 1");
+            db.InsertCell();
+            db.Write("This is Row 3 Cell 2");
+            db.InsertCell();
+            db.Write("This is Row 3 Cell 3");
+            db.EndRow();
+            db.EndTable();
 
             // Save our document into DOCX format.
-            dc.Save(documentPath);
+            string filePath = "Result.docx";
+            dc.Save(filePath);
 
             // Open the result for demonstration purposes.
-            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo(documentPath) { UseShellExecute = true });
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo(filePath) { UseShellExecute = true });
         }
     }
 }
