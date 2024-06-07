4bfe45b7a3c5f076d9bd652845eb7fe68072f626
MaxSautin <max.sautinsoft@gmail.com>
2024-06-07 15:06:39 +0400

    Updated examples.

---
+++ CSharp/02. Sections and Page Layout/07. Change page size and margins in DOCX document/Sample.cs
@@ -0,0 +1,41 @@
+using SautinSoft.Document;
+using System.IO;
+
+namespace Sample
+{
+    class Sample
+    {
+        static void Main(string[] args)
+        {
+            // Get your free 30-day key here:   
+            // https://sautinsoft.com/start-for-free/
+
+            ChangePageProperties();
+        }
+
+        /// <summary>
+        /// How to adjust a document page properties. 
+        /// </summary>
+        /// <remarks>
+        /// Details: https://sautinsoft.com/products/document/help/net/developer-guide/page-setup.php
+        /// </remarks>
+        public static void ChangePageProperties()
+        {
+            var inpFile = Path.GetFullPath(@"..\..\..\example.docx");
+            var outFile = Path.GetFullPath("Result.docx");
+            DocumentCore dc = DocumentCore.Load(inpFile);
+
+            // Apply page size, orientation and margins.
+            foreach (var sect in dc.Sections)
+            {
+                sect.PageSetup.PaperType = PaperType.A3;
+                sect.PageSetup.Orientation = Orientation.Landscape;
+                sect.PageSetup.PageMargins.Left = LengthUnitConverter.Convert(5.0, LengthUnit.Centimeter, LengthUnit.Point);
+            }
+
+            dc.Save(outFile);
+
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo(outFile) { UseShellExecute = true });
+        }
+    }
+}
